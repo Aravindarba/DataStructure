e38934ba85a17ac4f773d8f2d6f68a2ae06778f1
Aravind <3aravidarba@gmail.com>
2024-06-04 18:39:23 +0530

    Update in Switch17.java

--- src/main/java/org/example/Switch17.java
+++ src/main/java/org/example/Switch17.java
@@ -4,7 +4,6 @@ public class Switch17 {
 
  public static  void main(String args[]) {
      String day = "Wednesday";
-
      switch (day) {
          case "Monday", "Wednesday" -> System.out.println("Weekday");
          case "Sunday", "Saturday" -> System.out.println("Weekend");
